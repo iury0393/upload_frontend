377bf569372858b7b6f5287e3d2dda9c539d7ca2
Iury Vasconcelos <iury0393@gmail.com>
2020-07-21 20:00:57 -0300

    Criando a Dropzone na tela

--- src/components/Upload/index.js
+++ src/components/Upload/index.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
 import Dropzone from 'react-dropzone';
 
-// import { Container } from './styles';
+import { DropContainer, UploadMessage } from './styles';
 
 export default class Upload extends Component {
   render() {
     return (
       <Dropzone accept="image/*, application/pdf" onDropAccepted={() => {}}>
-
+        { ({ getRootProps, getInputProps, isDragActive, isDragReject }) => (
+          <DropContainer 
+            {...getRootProps()}
+            isDragActive={isDragActive}
+            isDragReject={isDragReject}
+          >
+            <input {...getInputProps()}/>
+          </DropContainer>
+        )}
       </Dropzone>
     );
   }
