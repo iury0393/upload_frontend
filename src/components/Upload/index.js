f3e3b0f0eecc557452389694bd30afb70a22a356
Iury Vasconcelos <iury0393@gmail.com>
2020-07-21 19:01:27 -0300

    Criando a estilização

---
+++ src/components/Upload/index.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+// import { Container } from './styles';
+
+export default class Upload extends Component {
+  render() {
+    return <div />;
+  }
+}
+
