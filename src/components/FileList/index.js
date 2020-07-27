7ad7a367db08449dbef41bb42c611058334bcc4f
Iury Vasconcelos <iury0393@gmail.com>
2020-07-27 10:57:13 -0300

    Finalizando a aplicação

--- src/components/FileList/index.js
+++ src/components/FileList/index.js
@@ -1,43 +1,57 @@
-import React from 'react';
+import React from "react";
 import { CircularProgressbar } from 'react-circular-progressbar';
-import { MdCheckCircle, MdError, MdLink } from 'react-icons/md'
+import { MdCheckCircle, MdError, MdLink } from "react-icons/md";
 
-import { Container, FileInfo, Preview } from './styles';
+import { Container, FileInfo, Preview } from "./styles";
 
-const FileList = () => (
+const FileList = ({ files, onDelete }) => (
   <Container>
-    <li>
-      <FileInfo>
-        <Preview src="" />
+    {files.map(uploadedFile => (
+      <li key={uploadedFile.id}>
+        <FileInfo>
+          <Preview src={uploadedFile.preview} />
+          <div>
+            <strong>{uploadedFile.name}</strong>
+            <span>
+              {uploadedFile.readableSize}{" "}
+              {!!uploadedFile.url && (
+                <button onClick={() => onDelete(uploadedFile.id)}>
+                  Excluir
+                </button>
+              )}
+            </span>
+          </div>
+        </FileInfo>
+
         <div>
-          <strong></strong>
-          <span> <button onClick={() => {}}>Exluir</button></span>
+          {!uploadedFile.uploaded &&
+            !uploadedFile.error && (
+              <CircularProgressbar
+                styles={{
+                  root: { width: 24 },
+                  path: { stroke: "#7159c1" }
+                }}
+                strokeWidth={10}
+                percentage={uploadedFile.progress}
+              />
+            )}
+
+          {uploadedFile.url && (
+            <a
+              href={uploadedFile.url}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <MdLink style={{ marginRight: 8 }} size={24} color="#222" />
+            </a>
+          )}
+
+          {uploadedFile.uploaded && <MdCheckCircle size={24} color="#78e5d5" />}
+          {uploadedFile.error && <MdError size={24} color="#e57878" />}
         </div>
-      </FileInfo>
-      <div>
-        <CircularProgressbar
-          styles={{
-            root: { width: 24},
-            path: { stroke: '#7159C1'}
-          }}
-          strokeWidth={10}
-          percentage={60}
-        />
-
-        <a
-          href="#"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <MdLink style={{ marginRight: 8 }} size={24} color="#222" />
-        </a>
-
-        <MdCheckCircle size={24} color="#78E5D5" />
-        <MdError size={24} color="#E57878" />
-      </div>
-    </li>
+      </li>
+    ))}
   </Container>
-
 );
 
 export default FileList;
--- src/components/Upload/index.js
+++ src/components/Upload/index.js
@@ -1,31 +1,34 @@
-import React, { Component } from 'react';
-import Dropzone from 'react-dropzone';
+import React, { Component } from "react";
 
-import { DropContainer, UploadMessage } from './styles';
+import Dropzone from "react-dropzone";
+
+import { DropContainer, UploadMessage } from "./styles";
 
 export default class Upload extends Component {
   renderDragMessage = (isDragActive, isDragReject) => {
-    if(!isDragActive) {
-      return <UploadMessage>Arraste arquivos aqui ...</UploadMessage>
+    if (!isDragActive) {
+      return <UploadMessage>Arraste arquivos aqui...</UploadMessage>;
     }
 
-    if(isDragReject) {
-      return <UploadMessage type="error">Arquivo não suportado</UploadMessage>
+    if (isDragReject) {
+      return <UploadMessage type="error">Arquivo não suportado</UploadMessage>;
     }
 
-    return <UploadMessage type="success">Solte os arquivos aqui</UploadMessage>
-  }
+    return <UploadMessage type="success">Solte os arquivos aqui</UploadMessage>;
+  };
 
   render() {
+    const { onUpload } = this.props;
+
     return (
-      <Dropzone accept="image/*, application/pdf" onDropAccepted={() => {}}>
-        { ({ getRootProps, getInputProps, isDragActive, isDragReject }) => (
-          <DropContainer 
+      <Dropzone accept="image/*" onDropAccepted={onUpload}>
+        {({ getRootProps, getInputProps, isDragActive, isDragReject }) => (
+          <DropContainer
             {...getRootProps()}
             isDragActive={isDragActive}
             isDragReject={isDragReject}
           >
-            <input {...getInputProps()}/>
+            <input {...getInputProps()} />
             {this.renderDragMessage(isDragActive, isDragReject)}
           </DropContainer>
         )}
